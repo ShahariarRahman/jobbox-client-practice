8040669dd4616fe44c7b2614dd70bc0a8a584093
ShahariarRahman <shahariarrahman98@gmail.com>
2023-02-06 03:22:48 +0600

    conditional rendering of sidebar of dashboard by role of user

--- src/layout/dashboard/Sidebar.js
+++ src/layout/dashboard/Sidebar.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
+import { useSelector } from "react-redux";
 const Sidebar = () => {
+  const {
+    user: { role },
+  } = useSelector((state) => state.auth);
+
+  const employerRoutes = [{ name: "Add job", path: "add-job" }];
+  const candidateRoutes = [{ name: "Applied jobs", path: "applied-jobs" }];
+
   return (
     <div className="bg-primary/10 col-span-2 h-screen sticky top-0">
       <ul className="flex flex-col gap-2 w-full h-full  p-3">
@@ -12,14 +20,28 @@ const Sidebar = () => {
           </Link>
           <h1 className="text-xl">Dashboard</h1>
         </div>
-        <li>
-          <Link
-            className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
-            to="add-job"
-          >
-            Add Job
-          </Link>
-        </li>
+        {role === "employer" &&
+          employerRoutes.map(({ name, path }, i) => (
+            <li key={i}>
+              <Link
+                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
+                to={`${path}`}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
+        {role === "candidate" &&
+          candidateRoutes.map(({ name, path }, i) => (
+            <li key={i}>
+              <Link
+                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
+                to={`${path}`}
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
