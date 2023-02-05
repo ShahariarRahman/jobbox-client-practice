183f4cf3de5bff9011aeb9a199b3b83ff5133dec
ShahariarRahman <shahariarrahman98@gmail.com>
2023-02-06 05:39:29 +0600

    jobData's data shown at UI

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,14 +1,18 @@
 import React from "react";
 import JobCard from "../components/reusable/JobCard";
+import { useGetJobsQuery } from "../features/job/jobApi";
 
 const Jobs = () => {
+  const { data } = useGetJobsQuery();
   return (
     <div className="pt-14">
       <div className="bg-primary/10 p-5 rounded-2xl">
         <h1 className="font-semibold text-xl">Find Jobs</h1>
       </div>
       <div className="grid grid-cols-2 gap-5 mt-5">
-        <JobCard />
+        {data?.data.map((job) => (
+          <JobCard key={job._id} jobData={job} />
+        ))}
       </div>
     </div>
   );
