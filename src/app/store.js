6f10a8ed84c9966d445dc93fe692f6318df901ff
ShahariarRahman <shahariarrahman98@gmail.com>
2023-02-06 02:02:57 +0600

    add reducer and middleware for base setup of rtk query in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import apiSlice from "../features/api/apiSlice";
 import authSlice from "../features/auth/authSlice";
 
 const store = configureStore({
   devTools: true,
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
