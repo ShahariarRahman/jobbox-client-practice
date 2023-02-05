a068db09c14d53b4b6720c86a98f79aabf2444d0
ShahariarRahman <shahariarrahman98@gmail.com>
2023-02-06 02:13:50 +0600

    register user in authApi using apiSlice.injectEndpoints

---
+++ src/features/auth/authApi.js
@@ -0,0 +1,13 @@
+import apiSlice from "../api/apiSlice";
+
+const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: "/user",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
